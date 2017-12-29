0280f01c38434b011b328ba66c891ff46bbbf729
Paul Keith <javelinanddart@gmail.com>
2017-12-29 01:00:35 +0100

    kltekor: Include fingerprint support from common (part 2)
    * Additionally, enforce the presence of BoardConfigCommon.mk
    
    Change-Id: I6c51987d20c4e58ac5401f01d0aedbf099be22f1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,12 +14,15 @@
 # limitations under the License.
 
 # inherit from common klte
--include device/samsung/klte-common/BoardConfigCommon.mk
+include device/samsung/klte-common/BoardConfigCommon.mk
 
 TARGET_OTA_ASSERT_DEVICE := klte,kltekor,kltektt,kltelgt,klteskt
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineage_kltekor_defconfig
+
+# Fingerprint
+include $(COMMON_PATH)/fingerprint/board.mk
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8974
